980b8140b87989350a08c1960e14c91d4a9e6c19
eunhyeong99 <whdmsgud8@naver.com>
2024-12-20 22:21:52 +0900

    feat: Component 구성.

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? selectedDay = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Calendar(
+              focusedDay: DateTime.now(),
+              onDaySelected: onDaySelected,
+              selectedDayPredicate: selectedDayPredicate,
+            ),
+            TodayBanner(
+              selectedDay: selectedDay!,
+              taskCount: 0,
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                child: ListView(
+                  children: [
+                    ScheduleCard(
+                      startTime: DateTime(2024,03,19,11),
+                      endTime: DateTime(2024,03,19,12),
+                      content: 'Flutter Strudy',
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(selectedDay, focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+    });
+  }
+
+  bool selectedDayPredicate(date) {
+    if (selectedDay == null) {
+      return false;
+    }
+    return date.isAtSameMomentAs(selectedDay!);
+  }
+}
